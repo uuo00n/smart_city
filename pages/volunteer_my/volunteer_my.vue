4cf99972ed087a01476d50c5c9c641c56e1b3cf2
uuo00_n <huangjunbo1107@outlook.com>
2023-12-19 10:22:17 +0800

    志愿服务我的活动由于无法新建数据页面其他部分完成

--- pages/volunteer_my/volunteer_my.vue
+++ pages/volunteer_my/volunteer_my.vue
@@ -1,6 +1,19 @@
 <template>
 	<view>
-		
+		<view style="padding: 10rpx;">
+			<view>
+				<uni-segmented-control :current="current" :values="classItem" @clickItem="onClickItem">
+				</uni-segmented-control>
+			</view>
+			<view class="content">
+				<uni-list v-if="listData.length != 0">
+					<uni-list-item title="" note="" v-for="(item,index) in listData"></uni-list-item>
+				</uni-list>
+				<view v-else style="padding: 50rpx; text-align: center;">
+					<text>暂无数据</text>
+				</view>
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -8,11 +21,38 @@
 	export default {
 		data() {
 			return {
-				
+				classItem: ["已完成", "已报名", "已取消"],
+				current: 0,
+				listData:[]
 			}
 		},
+		mounted() {
+			this.getListData()
+		},
 		methods: {
-			
+			onClickItem(e) {
+				if (this.current != e.currentIndex) {
+					this.current = e.currentIndex;
+				}
+				this.getListData(this.current)
+			},
+			getListData(e) {
+				console.log(this.current)
+				uni.request({
+					url: 'http://124.93.196.45:10001/prod-api/api/volunteer-service/activity/my-list/' + e,
+					method: 'GET',
+					header: {
+						Authorization: uni.getStorageSync("token")
+					},
+					data: {},
+					success: res => {
+						this.listData = res.data.rows
+						console.log(res)
+					},
+					fail: () => {},
+					complete: () => {}
+				});
+			}
 		}
 	}
 </script>
